875b3806814becfcc2d9c8986bb464139f7316d9
yhbsh <houssembousmaha.ksu@gmail.com>
2024-11-06 00:30:25 +0300

    add -p

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 TARGETS=("3d" "activity" "audio" "camera" "cube" "game" "list" "triangle" "video")
 
-mkdir build
+mkdir -p build
 for target in "${TARGETS[@]}"; do
     echo "Building $target..."
     make -C "apps/$target" > /dev/null

144852e63f90c7a511a6500abd56b8fccf24c6ab
yhbsh <houssembousmaha.ksu@gmail.com>
2024-11-20 01:21:56 +0300

    refactor & allow portrait

--- apps/hls/App.java
+++ apps/hls/App.java
@@ -7,25 +7,75 @@ import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Toast;
+import android.view.ViewGroup;
+import android.view.Gravity;
+import android.widget.FrameLayout;
 
 public class App extends Activity {
-    private MediaPlayer mediaPlayer;
+    private MediaPlayer player;
     private SurfaceView surfaceView;
+    private int videoWidth;
+    private int videoHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        FrameLayout frameLayout = new FrameLayout(this);
+        frameLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        
         surfaceView = new SurfaceView(this);
-        setContentView(surfaceView);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+        surfaceView.setLayoutParams(layoutParams);
+        
+        frameLayout.addView(surfaceView);
+        setContentView(frameLayout);
+
         SurfaceHolder surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                initializeMediaPlayer(holder);
+                releaseMediaPlayer();
+                player = new MediaPlayer();
+                player.setDisplay(holder);
+
+                player.setOnVideoSizeChangedListener((mp, width, height) -> {
+                    videoWidth = width;
+                    videoHeight = height;
+                    updateVideoSize(surfaceView.getWidth(), surfaceView.getHeight());
+                });
+
+                player.setOnPreparedListener(p -> {
+                    videoWidth = p.getVideoWidth();
+                    videoHeight = p.getVideoHeight();
+                    updateVideoSize(surfaceView.getWidth(), surfaceView.getHeight());
+                    p.start();
+                });
+
+                player.setOnErrorListener((mp, what, extra) -> {
+                    String errorMessage = "Error playing stream: what=" + what + ", extra=" + extra;
+                    Toast.makeText(App.this, errorMessage, Toast.LENGTH_LONG).show();
+                    releaseMediaPlayer();
+                    return true;
+                });
+
+                player.setOnCompletionListener(mp -> releaseMediaPlayer());
+
+                try {
+                    player.setDataSource(App.this, Uri.parse("http://192.168.1.187:8000/live/stream.m3u8"));
+                    player.prepareAsync();
+                } catch (Exception e) {
+                    Toast.makeText(App.this, "Failed to load stream", Toast.LENGTH_SHORT).show();
+                    releaseMediaPlayer();
+                }
             }
 
             @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if (player != null) {
+                    updateVideoSize(width, height);
+                }
+            }
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
@@ -34,51 +84,54 @@ public class App extends Activity {
         });
     }
 
-    private void initializeMediaPlayer(SurfaceHolder holder) {
-        releaseMediaPlayer(); // Ensure any previous instance is released
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setDisplay(holder);
-        mediaPlayer.setOnPreparedListener(MediaPlayer::start);
-        mediaPlayer.setOnErrorListener((mp, what, extra) -> {
-            String errorMessage = "Error playing stream: what=" + what + ", extra=" + extra;
-            Toast.makeText(App.this, errorMessage, Toast.LENGTH_LONG).show();
-            releaseMediaPlayer();
-            return true;
-        });
-        mediaPlayer.setOnCompletionListener(mp -> releaseMediaPlayer());
-        try {
-            mediaPlayer.setDataSource(this, Uri.parse("http://192.168.1.187:8000/live/stream.m3u8"));
-            mediaPlayer.prepareAsync();
-        } catch (Exception e) {
-            Toast.makeText(this, "Failed to load stream", Toast.LENGTH_SHORT).show();
-            releaseMediaPlayer();
+    private void updateVideoSize(int surfaceWidth, int surfaceHeight) {
+        if (player == null || videoWidth == 0 || videoHeight == 0 || surfaceWidth == 0 || surfaceHeight == 0) {
+            return;
         }
+
+        float videoAspect = (float) videoWidth / videoHeight;
+        float surfaceAspect = (float) surfaceWidth / surfaceHeight;
+
+        int newWidth, newHeight;
+
+        if (videoAspect > surfaceAspect) {
+            newWidth = surfaceWidth;
+            newHeight = (int) (surfaceWidth / videoAspect);
+        } else {
+            newHeight = surfaceHeight;
+            newWidth = (int) (surfaceHeight * videoAspect);
+        }
+
+        ViewGroup.LayoutParams layoutParams = surfaceView.getLayoutParams();
+        layoutParams.width = newWidth;
+        layoutParams.height = newHeight;
+        surfaceView.setLayoutParams(layoutParams);
     }
 
     private void releaseMediaPlayer() {
-        if (mediaPlayer != null) {
+        if (player != null) {
             try {
-                mediaPlayer.stop();
+                player.stop();
             } catch (Exception ignored) {}
-            mediaPlayer.release();
-            mediaPlayer = null;
+            player.release();
+            player = null;
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mediaPlayer != null) {
-            mediaPlayer.pause();
+        if (player != null) {
+            player.pause();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mediaPlayer != null) {
+        if (player != null) {
             try {
-                mediaPlayer.start();
+                player.start();
             } catch (Exception ignored) {}
         }
     }
